7bbb51d1dfc32449900846590c90d8b122d28298
jack <jack.kelling7@gmail.com>
2021-05-15 09:55:29 -0500

    Added body content, changed colors in CSS, added display date with Java

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+function showDate() {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //months are zero based
+    var curr_year = d.getFullYear();
+    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+  }
