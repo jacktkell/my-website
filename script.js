972c62a2512e4e224c35583100b6d2dc9435983d
jack <jack.kelling7@gmail.com>
2021-05-16 17:11:11 -0500

    edited CSS and added more to java

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function showDate() {
     var d = new Date();
     var curr_date = d.getDate();
-    var curr_month = d.getMonth() + 1; //months are zero based
+    var curr_month = d.getMonth() + 1;
     var curr_year = d.getFullYear();
     document.write(curr_date + "-" + curr_month + "-" + curr_year);
+  }
+
+  function bigImg(x) {
+    x.style.height="100px";
+    x.style.width="100px";
+  }
+
+  function normalImg(x) {
+    x.style.height = "50px";
+    x.style.width="50px";
   }
